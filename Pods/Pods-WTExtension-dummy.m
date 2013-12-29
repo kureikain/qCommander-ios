088260402a706366311c04e1c1db5df78ba42fa9
kureikain <kureikain@gmail.com>
2013-12-29 12:42:30 -0800

    Update Podfile to AFNetowrking 1.x

---
+++ Pods/Pods-WTExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WTExtension : NSObject
+@end
+@implementation PodsDummy_Pods_WTExtension
+@end
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -6,11 +6,23 @@
 // project.
 
 
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 3
+
 // Firebase
 #define COCOAPODS_POD_AVAILABLE_Firebase
 #define COCOAPODS_VERSION_MAJOR_Firebase 1
 #define COCOAPODS_VERSION_MINOR_Firebase 1
 #define COCOAPODS_VERSION_PATCH_Firebase 1
+
+// GVCache
+#define COCOAPODS_POD_AVAILABLE_GVCache
+#define COCOAPODS_VERSION_MAJOR_GVCache 1
+#define COCOAPODS_VERSION_MINOR_GVCache 0
+#define COCOAPODS_VERSION_PATCH_GVCache 1
 
 // MMDrawerController
 #define COCOAPODS_POD_AVAILABLE_MMDrawerController
@@ -53,4 +65,16 @@
 #define COCOAPODS_VERSION_MAJOR_NUI 0
 #define COCOAPODS_VERSION_MINOR_NUI 4
 #define COCOAPODS_VERSION_PATCH_NUI 0
+
+// WTExtension/UIImage+WTExtension
+#define COCOAPODS_POD_AVAILABLE_WTExtension_UIImage_WTExtension
+#define COCOAPODS_VERSION_MAJOR_WTExtension_UIImage_WTExtension 1
+#define COCOAPODS_VERSION_MINOR_WTExtension_UIImage_WTExtension 2
+#define COCOAPODS_VERSION_PATCH_WTExtension_UIImage_WTExtension 1
+
+// WTURLImageView
+#define COCOAPODS_POD_AVAILABLE_WTURLImageView
+#define COCOAPODS_VERSION_MAJOR_WTURLImageView 1
+#define COCOAPODS_VERSION_MINOR_WTURLImageView 1
+#define COCOAPODS_VERSION_PATCH_WTURLImageView 0
 
