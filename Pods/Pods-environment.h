c9bc4506973ed5b6baea8343b5ebf4ad57e3aad1
kureikain <kureikain@gmail.com>
2014-01-15 19:27:13 -0800

    Progress loader

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -24,6 +24,12 @@
 #define COCOAPODS_VERSION_MINOR_GVCache 0
 #define COCOAPODS_VERSION_PATCH_GVCache 1
 
+// HTProgressHUD
+#define COCOAPODS_POD_AVAILABLE_HTProgressHUD
+#define COCOAPODS_VERSION_MAJOR_HTProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_HTProgressHUD 1
+#define COCOAPODS_VERSION_PATCH_HTProgressHUD 1
+
 // WTExtension/UIImage+WTExtension
 #define COCOAPODS_POD_AVAILABLE_WTExtension_UIImage_WTExtension
 #define COCOAPODS_VERSION_MAJOR_WTExtension_UIImage_WTExtension 1
